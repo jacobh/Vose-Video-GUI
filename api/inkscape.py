ed4b09618d98240e5cd816c835617fd61a2b949d
Jason O'Neil <jason.oneil@gmail.com>
2011-10-03 17:05:36 +0800

    Added basic outline for inkscape api.
    Mostly no working code yet.

---
+++ api/inkscape.py
@@ -0,0 +1,39 @@
+# 
+
+def createMenuAndLabel(unitCode, unitTitle, lecturerName, videoTitle):
+    # Copy the Main menu and label from the template
+    pass
+
+def createSlide(slideTitle, slideTime, slideText):
+    # Create slide from the template
+    pass
+
+def openMenu():
+    # Open the menu for editing in Inkscape
+    #inkscape menu.svg
+    pass
+
+def openLabel():
+    # Open the menu for editing in Inkscape
+    #inkscape label.svg
+    pass
+
+def openSlide(slideName):
+    # Open the menu for editing in Inkscape
+    #inkscape $slideName.svg
+    pass
+
+def convertSlidesToPng():
+    # Read a folder, convert all SVG to PNG
+    # inkscape -f infile.svg -C -e outfile.png
+    pass
+    
+def convertMenuToPng():
+    # inkscape -f menu.svg -C -e menu.png
+    pass
+
+def convertLabelToPdf():
+    # Convert a single SVG file to PDF
+    # inkscape -f infile.svg -C -e outfile.png
+    pass
+
