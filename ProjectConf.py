9a5262b0deafb35d370993cbe396c3f8aa4ea248
Jason O'Neil <jason.oneil@gmail.com>
2011-10-06 10:18:14 +0800

    Added ProjectConf, for now using static variables.
    Basic usage shown in test.py

---
+++ ProjectConf.py
@@ -0,0 +1,7 @@
+class ProjectConf():
+    projects = {
+                'Jason': { 'path' : '/home/jason/' },
+                'Jacob': { 'path' : '/home/jacob/' },
+    }
+    currentProject = projects['Jason']
+    
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 import api.ffmpeg
+from ProjectConf import ProjectConf
 
-api.ffmpeg.transcodeToMP4("assets/sampledata/originals/file.MTS", "assets/sampledata/transcodes/file.MTS.MP4")
+print ProjectConf.currentProject['path']
+ProjectConf.currentProject = ProjectConf.projects['Jacob']
+print ProjectConf.currentProject['path']
+
+#api.ffmpeg.transcodeToMP4("assets/sampledata/originals/file.MTS", "assets/sampledata/transcodes/file.MTS.MP4")
